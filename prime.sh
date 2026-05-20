e1b4faa739718aa485ad6ec903f3be92d1757db1
Adithyaasok <adithya4842@gmail.com>
2026-05-20 22:47:33 +0530

    Create prime.sh

---
+++ prime.sh
@@ -0,0 +1,16 @@
+echo "enter the limit"
+read l
+
+for ((i=1;i<=$l;i++))
+do 
+count=0
+for ((j=1;j<=$i;j++))
+do
+if [ $(($i%$j)) -eq 0 ]; then
+count=$(($count+1))
+fi
+done
+if [ $count -eq 2 ]; then
+echo "$i"
+fi
+done
